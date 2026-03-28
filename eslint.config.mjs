4af4212753ff9c2b3db117acd9e355ef52b7be56
Sedem <sedem066@outlook.com>
2026-03-28 15:07:31 +0000

    Fix ESLint config and upgrade Next.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,3 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import eslintConfigNext from "eslint-config-next";
 
-const eslintConfig = defineConfig([
-  ...(Array.isArray(nextVitals) ? nextVitals : [nextVitals]),
-  ...(Array.isArray(nextTs) ? nextTs : [nextTs]),
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default eslintConfigNext();
